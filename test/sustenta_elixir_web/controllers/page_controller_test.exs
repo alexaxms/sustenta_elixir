e672b28472548165891c7b0139165c2bdd708ed1
alexaxms <alexaxmss@hotmail.com>
2019-04-28 20:24:34 -0400

    static site test

--- test/sustenta_elixir_web/controllers/page_controller_test.exs
+++ test/sustenta_elixir_web/controllers/page_controller_test.exs
@@ -5,4 +5,9 @@ defmodule SustentaElixirWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
+
+  test "Quiénes Somos page test", %{conn: conn} do
+    conn = get(conn, "/quienes_somos")
+    assert html_response(conn, 200) =~ "Quiénes Somos"
+  end
 end
